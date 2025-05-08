b7bd4b5e2a5e4b3387adfc58356ea31b309e92e5
john bloodborne <offse7@localhost.localdomain>
2025-05-08 18:24:43 +0700

    Fixed .env files. Landing page blade and controller created

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Debug</title>
+</head>
+<body>
+    <h1>Custom debug page</h1>
+</body>
+</html>
+
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.7',
-        'version' => '12.0.7.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0463f78f05424ce59af02f471ffda18a88f46474',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.7',
-            'version' => '12.0.7.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0463f78f05424ce59af02f471ffda18a88f46474',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
