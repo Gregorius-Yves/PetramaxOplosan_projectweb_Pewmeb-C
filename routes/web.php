9987c61c6a5c3c10d24a072760a587eee56c8f30
Yoshia Benedict Parasian <yoshiabp@gmail.com>
2025-05-12 20:29:31 +0700

    Update web.php
    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/laravel/login', '/login')->name('login');
+Route::redirect('/laravel/register', '/register')->name('register');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Route::get('/', [\App\Http\Controllers\LandingController::class, 'index']);
+
+require __DIR__.'/auth.php';
