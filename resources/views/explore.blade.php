5950037cecadf8c4bc43edd3b57abe62978f8110
Yoshia Benedict Parasian <yoshiabp@gmail.com>
2025-05-14 23:34:41 +0700

    Update explore.blade.php

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -17,14 +17,14 @@
             <span class="text-lg font-bold text-orange-600">inaRISK Web</span>
         </div>
         <!-- Tombol Akses Profil -->
-    <a href="{{ route('profile') }}">
-        <div class="w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-600">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.25a8.25 8.25 0 1115 0v.75H4.5v-.75z" />
-            </svg>
-        </div>
-    </a>
+        <a href="{{ route('settings') }}">
+            <div class="w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-600">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.25a8.25 8.25 0 1115 0v.75H4.5v-.75z" />
+                </svg>
+            </div>
+        </a>
     </header>
 
     {{-- Top Image Carousel --}}
